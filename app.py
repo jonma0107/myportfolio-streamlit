1bca349c749b28ec00ff9f6df5ca3ccca915f412
jonma0107 <jduplantier44@gmail.com>
2023-07-17 23:03:07 -0500

    [CREATE] header section

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="My Portfolio", page_icon=":tada:", layout="wide")
+
+# --- HEADER SECTION ---
+st.subheader("Hi, I am Jonathan Meza :wave:")
+st.title("Backend Developer from Colombia")
+st.write("I am passionate about finding ways to use Python and Django :snake:")
+st.write("[Learn More >>>](https://www.jonathanmeza.com.co)")
