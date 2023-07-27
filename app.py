0e8e9aaeb56d1e793d727329838ab7af6e607590
jonma0107 <jduplantier44@gmail.com>
2023-07-26 21:27:55 -0500

    [CREATE] Projects section

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from PIL import Image
 import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
@@ -16,8 +17,14 @@ def load_lottierurl(url):
 # animations here: https://lottiefiles.com/getting-started
 # lottie_header = load_lottierurl("https://lottie.host/529b8118-d6d6-44bc-b7e1-b5e95efe65b6/wGuM1kIVUZ.json")
 # lottie_header = load_lottierurl("https://lottie.host/6db12b0a-a80f-4f8b-b256-116dc39e2fea/7FTo39uCwx.json")
+# lottie_header = load_lottierurl("https://lottie.host/e5c828f1-36cd-4297-a5e0-89b0b1f8a154/ZqciHXPNuS.json")
+# lottie_header = load_lottierurl("https://lottie.host/225b30c5-5a13-4573-81da-4451ae06e6c4/NlOhWl7tyn.json")
 lottie_header = load_lottierurl("https://lottie.host/8c902199-8b9f-4fbc-a62d-0e0aa521dd28/FUzKZjM2AF.json")
 lottie_coding = load_lottierurl("https://lottie.host/caf3346a-1dc0-4c02-9860-cf96e7d66e76/stwyAojHpV.json")
+# img_contact_form = Image.open("images/logo-j.jpg")
+# img_lottie_animation = Image.open("images/logo-j.jpg")
+coming_soon_1 = load_lottierurl("https://lottie.host/225b30c5-5a13-4573-81da-4451ae06e6c4/NlOhWl7tyn.json")
+coming_soon_2 = load_lottierurl("https://lottie.host/225b30c5-5a13-4573-81da-4451ae06e6c4/NlOhWl7tyn.json")
 
 # --- HEADER SECTION ---
 with st.container():
@@ -56,4 +63,43 @@ with st.container():
         st.write("[GitHub >>> ](https://github.com/jonma0107)")
 
     with right_column:
-        st_lottie(lottie_coding, height=550, key="coding")    
+        st_lottie(lottie_coding, height=550, key="coding")   
+
+# ---- PROJECTS ----
+
+# ---- Project 1 ----
+
+with st.container():
+    st.write("---")
+    st.header("My Projects")
+    st.write("##")
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        # insert image
+        # st.image(img_lottie_animation)
+        st_lottie(coming_soon_1, height=300, key="comingsoon1")
+    with text_column:
+        st.subheader("Integrate Lottie Animations Inside Your Streamlit App")
+        st.write(
+            """
+            Learn how to use Lottie File in Streamlit
+            """
+        )
+        st.markdown("[Watch Video ...](https://www.youtube.com/live/Sj57a1mP_n0?feature=share)")
+
+# ---- Project 2 ----        
+
+with st.container():
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        # insert image
+        # st.image(img_lottie_animation)
+        st_lottie(coming_soon_2, height=300, key="comingsoon2")
+    with text_column:
+        st.subheader("Integrate Lottie Animations Inside Your Streamlit App")
+        st.write(
+            """
+            Learn how to use Lottie File in Streamlit
+            """
+        )
+        st.markdown("[Watch Video ...](https://www.youtube.com/live/Sj57a1mP_n0?feature=share)")             
